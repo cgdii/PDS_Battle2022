7e91c0913407ca1dea418e2668e4e19ca8a50b42
James Brundage <james.brundage@start-automating.com>
2022-04-28 15:18:27 -0700

    Updating tests

--- PDS_Battle2022.tests.ps1
+++ PDS_Battle2022.tests.ps1
@@ -1,5 +1,12 @@
 describe "Our Module" {
-    context "Get-DiskUsage" {
+    it "Get-DiskUsage" {        
         Get-DiskUsage | Select-Object -ExpandProperty Size |Should -begreaterthan 0
+    }
+
+    it 'Gets uptime' {
+        Get-upTime | 
+            Select-Object -expand Uptime | 
+            Select-object -expand Totalseconds | 
+            Should -BeGreaterthan 1
     }
 }
