5cab9cf3b787d268a69a376d6c05762ef5bc3063
James Brundage <james.brundage@start-automating.com>
2022-04-28 15:06:32 -0700

    Fixing DiskUsage (adding formatting #8)

--- get-diskusagedemo.ps1
+++ get-diskusagedemo.ps1
@@ -1,3 +1,4 @@
+function Get-DiskUsage {
 <#
 .SYNOPSIS
 This script will generate a report showing the disk utilization on the system specified.
@@ -23,12 +24,11 @@ A table displayed is the conole will show the disk usage
 param (
   [string]$computername = 'localhost'
 )
-Get-WmiObject -class Win32_LogicalDisk -computername $computername `
- -filter "drivetype=3" |
+Get-WmiObject -class Win32_LogicalDisk -computername $computername -filter "drivetype=3" |
  Sort-Object -property DeviceID |
- Format-Table -property DeviceID,
-     @{label='FreeSpace(MB)';expression={$_.FreeSpace / 1MB -as [int]}},
-     @{label='Size(GB';expression={$_.Size / 1GB -as [int]}},
-     @{label='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
-
-     
+ Foreach-Object {
+    $_.pstypenames.clear()
+    $_.pstypenames.add('DiskSpace')
+    $_
+ }
+}
